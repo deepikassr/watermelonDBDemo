4300bc51b7b36c4011fb139aacbe7a693031e993
deepikassr <deepikareddy536@gmail.com>
2023-03-02 17:55:39 +0530

    first commit

---
+++ src/Db/DAO/PostDAO.js
@@ -0,0 +1,22 @@
+// --------------- ASSETS ---------------
+import { database } from '../db';
+
+const posts = database.collections.get('posts');
+console.log("posts sumanth..")
+console.log(posts)
+export default {
+    observePosts: () => posts.query().observe(),
+    createPost: async ({ title, body }) => {
+        await database.action(async () => {
+            await posts.create((post) => {
+                post.title = title;
+                post.body = body;
+            });
+        });
+    },
+    deleteAll: async () => {
+        await database.action(async () => {
+            await posts.query().destroyAllPermanently();
+        });
+    },
+};
